c086f4dcfd797b9ea84e0ea3932343f3a2a1656a
Caiohvboas <116687690+Caiohvboas@users.noreply.github.com>
2025-01-10 00:46:46 -0300

    Updated download button to trigger CV download from Google Drive link.

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,37 +1,32 @@
-import { Box, Container, Grid, styled, Typography } from "@mui/material"
-import Avatar from "../../../../assets/images/avatar.jpg"
+import { Box, Container, Grid, styled, Typography } from "@mui/material";
+import Avatar from "../../../../assets/images/avatar.jpg";
 import DownloadIcon from '@mui/icons-material/Download';
 import EmailIcon from '@mui/icons-material/Email';
 import StyledButton from "../../../../components/StyledButton/StyledButton";
 import { AnimatedBackground } from "../../../../components/AnimatedBackground/AnimatedBackground";
 
-
-
-
 const Hero = () => {
-
-    const StyledHero = styled("div")(({theme}) => ({
+    const StyledHero = styled("div")(({ theme }) => ({
         backgroundColor: theme.palette.primary.main,
         height: "100vh",
-        display:"flex",
+        display: "flex",
+        // zIndex: 10, // Garantindo que o Hero fique acima do About
+        // boxShadow: "0 30px 60px rgba(255, 0, 0, 1)", // Aumentando a sombra vermelha para ser mais visível
         alignItems: "center",
-        [theme.breakpoints.up('xs')]: { // <= mobile
+        [theme.breakpoints.up('xs')]: {
             paddingTop: "100px",
-
         },
-        [theme.breakpoints.up('md')]: { // >=mobile
+        [theme.breakpoints.up('md')]: {
             paddingTop: "0",
         }
-        
-        
-    }))
+    }));
 
-    const StyledImg = styled("img")(({theme}) => ({
+    const StyledImg = styled("img")(({ theme }) => ({
         width: "75%",
         borderRadius: "50%",
-         border: `1px solid ${theme.palette.primary.contrastText}`
-       
-    }))
+        border: `1px solid ${theme.palette.primary.contrastText}`
+        
+    }));
 
     return (
         <>
@@ -39,7 +34,7 @@ const Hero = () => {
                 <Container maxWidth="lg">
                     <Grid container spacing={2}>
                         <Grid item xs={12} md={5}>
-                        <Box position="relative">
+                            <Box position="relative">
                                 <Box position="absolute" width={"150%"} top={-100} right={0}>
                                     <AnimatedBackground />
                                 </Box>
@@ -51,29 +46,26 @@ const Hero = () => {
                         <Grid item xs={12} md={7}>
                             <Typography color="primary.contrastText" variant="h1" textAlign="center" pb={2}>Caio Henrique</Typography>
                             <Typography color="primary.contrastText" variant="h2" textAlign="center">Desenvolvedor web</Typography>
-                            <Grid container display='flex' justifyContent='center' spacing={3} pt={3}>
+                            <Grid container display='flex' justifyContent='center' spacing={3} pt={3} mb={4}>
                                 <Grid item xs={12} md={4} display="flex" justifyContent="center">
-                                    <StyledButton onClick={() => console.log("download")}>
-                                    <DownloadIcon />
-                                    <Typography>Donwload CV</Typography>
+<StyledButton onClick={() => window.open("https://drive.google.com/uc?export=download&id=19JmuRs8XG4ioQnMzMz9INmCnED_nFx61", "_blank")}>
+                                        <DownloadIcon />
+                                        <Typography>Donwload CV</Typography>
                                     </StyledButton>
                                 </Grid>
                                 <Grid item xs={12} md={4} display="flex" justifyContent="center">
                                     <StyledButton onClick={() => console.log("contact")}>
                                         <EmailIcon />
-                                        <Typography>Contact me</Typography>
+                                        <Typography>Contactme</Typography>
                                     </StyledButton>
                                 </Grid>
                             </Grid>
-
-
                         </Grid>
                     </Grid>
                 </Container>
-
             </StyledHero>
         </>
-    )
-}
+    );
+};
 
-export default Hero
+export default Hero;
