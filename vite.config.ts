30a45a61bdb6512e25dc0e29fd28f5d6d499eadc
Caiohvboas <116687690+Caiohvboas@users.noreply.github.com>
2025-01-10 03:30:14 -0300

    Updates

--- vite.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: '/my-portfolio/', // Caminho base relativo ao seu repositório no GitHub Pages
-  // outras configurações
-});
